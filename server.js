d3f804670f4bcd2e42a20f15227d4f8a6f7f6a2c
fo-rgb <fodemakale18@gmail.com>
2021-06-04 10:08:16 +0200

    mise a jour de node message-challenge

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
+const { response } = require("express");
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-
-app.use(cors());
+const fs = require("fs");
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -20,6 +19,32 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
+app.get("/messages", function (request, response) {
+  response.send(messages);
+});
+
+// Post
+
+app.post("/messages", function (req, res) {
+  let mesagePost = req.body;
+  console.log(mesagePost);
+  mesagePost.id = Math.max(...messages.map((j) => j.id)) + 1;
+  messages.push(mesagePost);
+  res.status(201).json(mesagePost);
+});
+
+// put
+app.put("/messages/id", function (req, res) {
+  let id = parseInt(req.params.id);
+  let body = req.body;
+  let messageFind = quotes.find((i) => i.id == id);
+  messageFind.from = body.from;
+  messageFind.text = body.text;
+  messageFind.id = body.id;
+
+  res.status(200).json(messageFind);
+});
+
 app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+  console.log("Listening on port 3000");
+});
